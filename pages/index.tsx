917622c2889e65c54f843af6e0c4b4ae45a35f1c
kahigash <131256919+kahigash@users.noreply.github.com>
2025-09-12 18:06:34 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@
 import { useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
 import { LanguageSwitcher } from '../components/LanguageSwitcher';
+import FeedbackCards from '../components/FeedbackCards';
+import FeedbackCardsTabs from '../components/FeedbackCardsTabs';
 
 type Role = 'user' | 'assistant';
 
